3467d6b25b4f2c2716e4d9406e20dfef35d4a0e9
Lili-Mae Edwards <lilimae@hotmail.co.uk>
2019-12-27 17:21:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -64,7 +64,10 @@ def naming_system
     sub_category: {
     label: "Phylum",
      sub_category: {
-       label: "Class"
+       label: "Class",
+       sub_category: {
+         label: "Order"
+       }
      }
    }
  }
